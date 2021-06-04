0cf0a9dfe364a99c12a1e2730cf9f72a8b16c2a0
yirideveloper <topyiri1113@gmail.com>
2021-06-04 22:05:54 +0000

    feat - switch to yarn, update package for client

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -3,10 +3,8 @@ import { connectRouter } from "connected-react-router";
 
 import user from "./user";
 
-const createRootReducer = (history) =>
+export default (history) =>
   combineReducers({
     router: connectRouter(history), // Connects react router to redux
     user,
   });
-
-export default createRootReducer;
